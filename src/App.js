ead1aaa4376c1b5c7288613f5f1cd79d16f6b249
João Reis <reisjoao97@hotmail.com>
2021-04-30 00:32:11 +0100

    Removed unused files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Login from "./features/Login";
 import AdminDashboard from "./features/AdminDashboard";
-import Welcome from "./OLDviews/Welcome";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -11,7 +10,6 @@ const App = () => {
     return (
             <Provider store={store}>
                 <Router>
-                    <Route exact path="/" component={Welcome} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/adminDashboard" component={AdminDashboard} />
                 </Router>
